11f7b54f8ea07532b8257d58ad806d6f3b587b61
hilazit <hila2468>
2015-04-01 15:02:58 +0300

    ex02 complete

--- src/com/mta/javacourse/Hila_NasdaqServlet.java
+++ src/com/mta/javacourse/Hila_NasdaqServlet.java
@@ -5,7 +5,11 @@ import javax.servlet.http.*;
 @SuppressWarnings("serial")
 public class Hila_NasdaqServlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-		resp.setContentType("text/plain");
-		resp.getWriter().println("Hello, world");
+		resp.setContentType("text/html");
+		int num1=3, num2=4, num3=7;
+		int sum=(num1+num2)*num3;
+		String resultStr = new String("<h1> Result of (" +
+				 + num1 + "+" + num2 + ")*" + num3 + " = " + sum + "</h1>");
+		resp.getWriter().println(resultStr);
 	}
 }
